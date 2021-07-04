45add965f69caaf4a6437fb3381318b948b11aa9
GustavoAdami <gustavo_adami@hotmail.com>
2021-07-04 11:56:43 -0400

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c2b0e700e16567b7e0f5771594ccf561&query=' + latitude + ',' + longitude + '&units=m';
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error){
+            callback('Unable to connect to Weatherstack service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress. It feels like ' + body.current.feelslike + ' degrees out.')
+        }            
+    })
+}
+
+module.exports = forecast
